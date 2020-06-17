0000ef3751f78d86e0d849b09ffeee80cd025739
gorirakuniro <iwigenki0926@gmail.com>
2020-06-17 12:04:28 +0900

    Updates

---
+++ precache-manifest.a690481d50780d11750242fb495de2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9235150a4d51547304703c3c849ed85e",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "c98a6f6122f9b99288cb",
+    "url": "/Pokemon/static/css/main.cbc79103.chunk.css"
+  },
+  {
+    "revision": "ad75e22dd8aa62289124",
+    "url": "/Pokemon/static/js/2.ac72972a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Pokemon/static/js/2.ac72972a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98a6f6122f9b99288cb",
+    "url": "/Pokemon/static/js/main.2d57d8ed.chunk.js"
+  },
+  {
+    "revision": "88f10aed6ed0ea6669b1",
+    "url": "/Pokemon/static/js/runtime-main.74e75df6.js"
+  }
+]);
